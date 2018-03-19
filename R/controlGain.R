91ec8dedea0500bf07421b614ef3c5dda7a0f288
Jessica Rutkoski <jer263@cornell.edu>
2018-03-19 09:36:49 +0800

    change season number to year

--- R/controlGain.R
+++ R/controlGain.R
@@ -11,17 +11,17 @@
 
 controlGain<- function(dat, label='', tunit='units',minNumb=10,minNumbCk=3){
   #Check the column names
-  if(any(colnames(dat)!= c("gid", "blue","se","season_number")))
-    stop("data should contain gid, blue, se, and season_number columns")
+  if(any(colnames(dat)!= c("gid", "blue","se","year")))
+    stop("data should contain gid, blue, se, and year columns")
   dat$gid<- as.character(dat$gid)
-  dat$season_number<- as.numeric(dat$season_number)
+  dat$year<- as.numeric(dat$year)
   dat$blue<- as.numeric(dat$blue)
   dat$se<- as.numeric(dat$se)
 
-  #check the dataset for duplicate gids within a season
-  id_sea<- paste(dat$gid, dat$season_number)
+  #check the dataset for duplicate gids within a year
+  id_sea<- paste(dat$gid, dat$year)
   if(!length(unique(id_sea))==length(id_sea))
-    stop("Error: There should be only one value per genotype per season number")
+    stop("Error: There should be only one value per genotype per year")
 
   #identify which are the potential checks
   tb<- table(dat$gid)
@@ -30,9 +30,9 @@ controlGain<- function(dat, label='', tunit='units',minNumb=10,minNumbCk=3){
     stop("Error: There are no checks in the dataset that have been used for the minimum number of years")
   ckcand<- names(ckcand)
 
-  #discard earlier seasons where the checks are not sufficient
+  #discard earlier years where the checks are not sufficient
   dck<-dat[which(dat$gid %in% ckcand),]
-  octab<- reshape::cast(dck, gid~season_number, value='blue')
+  octab<- reshape::cast(dck, gid~year, value='blue')
   octab2<- octab[,-1]
   tally<- apply(octab2, 2, function(x)length(na.omit(x)))
   colrm<- which(tally<minNumbCk)
@@ -43,20 +43,20 @@ controlGain<- function(dat, label='', tunit='units',minNumb=10,minNumbCk=3){
   }
 
   if(start_num>ncol(octab2))
-    stop("Error: There needs to be at least common 3 checks used for the past 10 seasons by default")
+    stop("Error: There needs to be at least common 3 checks used for the past 10 years by default")
   sea_sel<- colnames(octab2)[start_num:ncol(octab2)]
   nsea<- length(sea_sel)
 
   if(nsea<minNumb)
-    stop("Error: There needs to be at least common 3 checks used for the past 10 seasons by default")
+    stop("Error: There needs to be at least common 3 checks used for the past 10 years by default")
 
-  #select the seasons with sufficent checks for the analysis
+  #select the years with sufficent checks for the analysis
   sea_sel<- as.numeric(sea_sel)
-  dat<- dat[which(dat$season_number %in% sea_sel),]
+  dat<- dat[which(dat$year %in% sea_sel),]
 
   #identify the checks
   tb<- table(dat$gid)
-  tbsel<- tb[which(tb==length(unique(dat$season_number)))]
+  tbsel<- tb[which(tb==length(unique(dat$year)))]
   cks<- names(tbsel)
 
   #add the check indicator column
@@ -65,47 +65,47 @@ controlGain<- function(dat, label='', tunit='units',minNumb=10,minNumbCk=3){
   dat$Population<- factor(dat$Population, levels=c('Control', 'Selected'))
 
   #keep earliest occurance of varieties only
-  dat<- dat[order(dat$season_number),] #order by season
+  dat<- dat[order(dat$year),] #order by year
   selgids<- unique(dat$gid[which(dat$Population=='Selected')])
   ixkp<- c(which(dat$Population=='Control'), match(selgids, dat$gid))
   dat<- dat[ixkp, ]
 
   #start time at 0
-  mnsea<- min(dat$season_number)
-  dat$season_number<- dat$season_number-mnsea
+  mnsea<- min(dat$year)
+  dat$year<- dat$year-mnsea
 
   #fit model to get points for genetic value (stage 1)
-  dat$season_number<- as.character(dat$season_number)
+  dat$year<- as.character(dat$year)
   wtMod0<- 1/(dat$se/max(dat$se))
-  mod0<- lm(blue~ Population+season_number+Population:season_number, weights=wtMod0, data=dat)
-  lsm<- lsmeans::lsmeans(mod0, specs='Population', by='season_number', cov.reduce=FALSE)
+  mod0<- lm(blue~ Population+year+Population:year, weights=wtMod0, data=dat)
+  lsm<- lsmeans::lsmeans(mod0, specs='Population', by='year', cov.reduce=FALSE)
   lsm<- lsmeans::contrast(lsm, method='trt.vs.ctrl')
   pts<- as.data.frame(summary(lsm))
 
   #genetic trend model and fitted values (stage 2)
-  pts$season_number<- as.numeric(as.character(pts$season_number))
+  pts$year<- as.numeric(as.character(pts$year))
   wt<- 1/(pts$SE/max(pts$SE))
-  mdG<-lm(estimate~season_number, weights=wt, data=pts)
-  lsm<- lsmeans::lsmeans(mdG, specs='season_number', cov.reduce=FALSE)
+  mdG<-lm(estimate~year, weights=wt, data=pts)
+  lsm<- lsmeans::lsmeans(mdG, specs='year', cov.reduce=FALSE)
   genEst<- as.data.frame(summary(lsm))
-  pts$season_number<-pts$season_number+mnsea #Gen-val tab
+  pts$year<-pts$year+mnsea #Gen-val tab
 
   #get the points for the other plots (stage 1)
-  lsm<- lsmeans::lsmeans(mod0, specs='season_number', by='Population', cov.reduce=FALSE)
+  lsm<- lsmeans::lsmeans(mod0, specs='year', by='Population', cov.reduce=FALSE)
   ptsPop<- as.data.frame(summary(lsm))
 
   #Phenotypic trends (stage 2)
-  ptsPop$season_number<- as.numeric(as.character(ptsPop$season_number))
+  ptsPop$year<- as.numeric(as.character(ptsPop$year))
   wt<- 1/(ptsPop$SE/max(ptsPop$SE))
-  mdPS<-lm(lsmean~season_number+Population+Population:season_number, data=ptsPop, weights=wt)
-  lsm<- lsmeans::lsmeans(mdPS, specs='Population', by='season_number' ,cov.reduce=FALSE)
+  mdPS<-lm(lsmean~year+Population+Population:year, data=ptsPop, weights=wt)
+  lsm<- lsmeans::lsmeans(mdPS, specs='Population', by='year' ,cov.reduce=FALSE)
   fitted<- as.data.frame(summary(lsm))
-  ptsPop$season_number<- ptsPop$season_number+mnsea
-  fitted$season_number<- fitted$season_number+mnsea
+  ptsPop$year<- ptsPop$year+mnsea
+  fitted$year<- fitted$year+mnsea
 
   #create the phenotypic trend plot
-  dat$season_number<- as.numeric(dat$season_number)+mnsea
-  p1<- ggplot(data=ptsPop, aes(x=season_number, y=lsmean, group=Population)) +
+  dat$year<- as.numeric(dat$year)+mnsea
+  p1<- ggplot(data=ptsPop, aes(x=year, y=lsmean, group=Population)) +
     theme_minimal()+
     geom_point(size=2,stroke=1,aes(shape=Population, color=Population))+
     scale_color_manual(values=c('slategray4', 'darkorange'))+
@@ -115,22 +115,22 @@ controlGain<- function(dat, label='', tunit='units',minNumb=10,minNumbCk=3){
     scale_linetype_manual(values=c("longdash", "twodash"))+
     geom_ribbon(data=fitted, aes(ymin=lower.CL, ymax=upper.CL, fill=Population), alpha=0.2)+
     scale_fill_manual(values=c('slategray4', 'darkorange'), name="fill")+
-    labs(y = paste("Phenotypic value in", tunit, sep=" "), x='Season number')+
+    labs(y = paste("Phenotypic value in", tunit, sep=" "), x='year')+
     ggtitle(paste("Phenotypic trends", label, sep=""))+
     theme(legend.position="top",plot.title = element_text(hjust = 0.5))+
     guides(fill = "none")
 
   #create the genetic trend plot
   #geom_point(color='slategray4', size=2, shape=0)+geom_line(color='black')+
-  genEst$season_number<- genEst$season_number+mnsea
+  genEst$year<- genEst$year+mnsea
   colnames(pts)[3]<- 'lsmean'
-  p2<- ggplot(pts,aes(x=season_number, y=lsmean)) +
-    geom_point(shape=1, size=2,stroke=1, color='slategray4', data=pts, aes(x=season_number,y=lsmean))+
+  p2<- ggplot(pts,aes(x=year, y=lsmean)) +
+    geom_point(shape=1, size=2,stroke=1, color='slategray4', data=pts, aes(x=year,y=lsmean))+
     geom_line(data=genEst, color='slategray4', linetype='longdash')+
     geom_errorbar(color='slategray4', data=pts, size=1, aes(ymin=lsmean-SE, ymax=lsmean+SE, width=0.2))+
     theme_minimal()+
     geom_ribbon(data=genEst, aes(ymin=lsmean-SE, ymax=lsmean+SE), fill='slategray4', alpha=0.2)+
-    labs(y = paste("Predicted average genetic value in", tunit,sep=" "), x='Season number')+
+    labs(y = paste("Predicted average genetic value in", tunit,sep=" "), x='year')+
     ggtitle(paste("Predicted genetic trend", label, sep=""))+
     theme(plot.title = element_text(hjust = 0.5))
 
@@ -139,11 +139,11 @@ controlGain<- function(dat, label='', tunit='units',minNumb=10,minNumbCk=3){
   psTab<- as.data.frame(summary(mdPS)$coefficients)
 
   cfnms<-c(paste('Control baseline in', tunit, sep=" "),
-                paste('Control trend in', tunit, "per season number", sep=" "),
+                paste('Control trend in', tunit, "per year", sep=" "),
                 paste('Selected baseline in', tunit, sep=" "),
-                paste('Selected-control trend in', tunit, "per season number", sep=" "),
+                paste('Selected-control trend in', tunit, "per year", sep=" "),
                 paste('Predicted genetic value baseline in', tunit, sep=" "),
-                paste('Predicted genetic trend in', tunit, "per season number", sep=" "))
+                paste('Predicted genetic trend in', tunit, "per year", sep=" "))
   rslts<- data.frame(Model=c(rep('P',4),
              rep('G',2)),cfnms, rbind(psTab, genTab))
   row.names(rslts)<- c(1:nrow(rslts))
@@ -155,8 +155,8 @@ controlGain<- function(dat, label='', tunit='units',minNumb=10,minNumbCk=3){
   #make anova table into a dataframe
   avP<- as.data.frame(anova(mdPS))
   avG<- as.data.frame(anova(mdG))
-  varnms<- c('Season number', 'Population', 'Season number x population',
-             'Residuals', 'Season number', 'Residuals')
+  varnms<- c('year', 'Population', 'year x population',
+             'Residuals', 'year', 'Residuals')
 
   av<- data.frame(Model=c(rep('P',4),rep('G',2)), varnms, rbind(avP, avG))
   row.names(av)<- c(1:nrow(av))
