bed6e319f97ec2e3b8f4cc7af4ac917b894c8ff8
Jessica Rutkoski <jer263@cornell.edu>
2018-04-15 13:42:13 +0800

    add random number seed

--- R/demoSelection.R
+++ R/demoSelection.R
@@ -12,7 +12,7 @@
 demoSelection<- function(pop0min= 0, pop0max= 7, 
                          herit= 0.1, popsize= 100, 
                         numparents= 20, ncycles= 2, 
-                        cycledur=7){
+                        cycledur=7, rnseed=99){
 
 if(numparents>popsize){
   numparents=popsize
